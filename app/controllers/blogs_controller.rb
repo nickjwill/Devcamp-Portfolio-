1ef3e54c9236bad5048644aa82a3c8db28edc2d2
Nicholas  Williams <nickwilliams@Nicholass-MacBook-Pro.local>
2017-10-26 18:55:43 -0600

    Customized the design for the blog index page

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blog"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit, :toggle_status]}, site_admin: :all
 
 
   # GET /blogs
@@ -64,6 +64,15 @@ class BlogsController < ApplicationController
     end
   end
 
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+
+    redirect_to blogs_url, notice: 'Post status has been updated'
+
   private
     def set_blog
       @blog = Blog.friendly.find(params[:id])
@@ -73,4 +82,6 @@ class BlogsController < ApplicationController
     def blog_params
       params.require(:blog).permit(:title, :body)
     end
+  end
 end
+

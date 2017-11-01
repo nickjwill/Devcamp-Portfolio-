5156795553e571cef16a4c38c104191d35d6a54f
Nicholas  Williams <nickwilliams@Nicholass-MBP.ad.uvu.edu>
2017-11-01 10:44:38 -0600

    Updated file upload forms and refactored placeholder

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,7 +2,7 @@ class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
   access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
-
+  
   def index
     @portfolio_items = Portfolio.by_position
   end
@@ -10,10 +10,10 @@ class PortfoliosController < ApplicationController
   def sort
     params[:order].each do |key, value|
       Portfolio.find(value[:id]).update(position: value[:position])
-  end
+    end
 
-  render nothing: true 
-end
+    render nothing: true
+  end
 
   def angular
     @angular_portfolio_items = Portfolio.angular
@@ -29,7 +29,7 @@ end
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your Portfolio item is now live.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
@@ -37,13 +37,9 @@ end
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
-    3.times { @portfolio_item.technologies.build }
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -54,35 +50,31 @@ end
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
-    # Perform the Lookup
-      @portfolio_item = Portfolio.find(params[:id])
-    
     # Destroy/delete the record
-      @portfolio_item.destroy
-    
+    @portfolio_item.destroy
+
     # Redirect
-      respond_to do |format|
-        format.html { redirect_to blogs_url, notice: 'Record was removed.' }
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
 
   private
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, 
-                                      :subtitle, 
-                                      :body, 
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
                                       :main_image,
                                       :thumb_image,
                                       technologies_attributes: [:name]
-                                      )
+                                     )
   end
 
-    def set_portfolio_item
+  def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
 end

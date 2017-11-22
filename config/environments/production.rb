9f6aacf18ba3dcdbaac1a918e1c1975a71738c0c
nickjwill <nicholasjwilliams1@gmail.com>
2017-11-21 22:10:48 -0700

    Intergrated production calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://njw-devportfolio.herokuapp.com', 'http://njw-devportfolio.herokuapp.com']
+config.action_cable.url = "wss://njw-devportfolio.herokuapp.com/cable"
 end
